e86e9b0a9fe275bfd5debb050094591d19fac7ed
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2023-05-26 10:58:08 +0200

    Added dashboard to transform Twitter query into API url

--- u2p050twitterfetcher/dash/callbacks.py
+++ u2p050twitterfetcher/dash/callbacks.py
@@ -1,15 +1,46 @@
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from ..auth.auth import payload_from_token
 from ..dash.cache.helpers import store_data_frame, load_data_frame
-
+from dash import html, dcc
+import urllib.parse
+from flask import current_app
+import os
 
 def register_callbacks(app, cache=None):
-    @app.callback()
-    def first_callback():
-        """
-        """
 
-        # Way to secure function
-        payload = payload_from_token()
+    @app.callback(
+        Output(component_id='conversation', component_property='children'),
+        Input(component_id='send_button', component_property='n_clicks'),
+        Input(component_id="msg_input",component_property="n_submit"),
+        State(component_id='msg_input', component_property='value'),
+        prevent_initial_call=True
+    )
+    # function to add new user*bot interaction to conversation history
+    def update_conversation(click, n_submit, text):
+        # user message aligned left
+        rcvd = [dcc.Markdown(f'*{text}*', style={'text-align': 'left'})]
 
-        return()
+        r = urllib.parse.quote_plus(text)
+
+        # bot response aligned right and italics
+        rspd = [dcc.Markdown(f'**{r}**', style={'text-align': 'left'})]
+
+        base_url = current_app.config.get("HEROKU_URL") if current_app.config.get("HEROKU_URL") else "http://127.0.0.1:5000"
+        base_url = os.path.join(base_url, "api", "fetch", "twitter")
+        full_url = base_url + "?content=" + r
+
+        # Create a clickable link
+        clickable_link = html.A(f'{full_url}', href=f'{full_url}', target='_blank')
+
+        output = rcvd + rspd + [html.Hr(), clickable_link]
+
+        return output
+
+
+
+    @app.callback(
+        Output(component_id='msg_input', component_property='value'),
+        [Input(component_id='conversation', component_property='children')]
+    )
+    def clear_input(_):
+        return ''
--- u2p050twitterfetcher/dash/layout.py
+++ u2p050twitterfetcher/dash/layout.py
@@ -1,4 +1,5 @@
 import dash_bootstrap_components as dbc
+from dash import html, dcc
 
 def apply_layout_with_auth(app):
     """Because Layout must be a dash component or a function that returns a dash component.
@@ -14,6 +15,22 @@ def layout():
         layout object
     """
 
-    layout = dbc.Container()
+    layout = html.Div([
+        html.Div([
+            html.Div([
+                html.P("Please enter your Twitter query:", style={'text-align': 'center'}),  # Instructions
+                html.Br(),
+                dcc.Input(id='msg_input', value='hello', type='text', debounce=True, n_submit=0, 
+                        style={'flex': '12', 'height': '40px', 'margin-right': '10px'}),
+                html.Button('Send', id='send_button', type='submit',
+                            style={'flex': '1', 'height': '40px'})
+                ],
+                style={'display': 'flex', 'width': '90%', 'margin': '0 auto', 'margin-bottom': '10px'}),
+            html.Br(),
+            html.Div(id='conversation', style={'width': '90%', 'margin': '0 auto', 'overflow': 'auto'}),
+            ],
+            id='screen',
+            style={'width': '100%', 'margin': '0 auto'})
+    ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'height': '100vh'})
 
     return layout
